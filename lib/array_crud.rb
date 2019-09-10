54f337e750bb8f636ea54e19c4919f95db2e40b7
Brad Farberman <bfarberman@gmail.com>
2019-09-10 00:48:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  meters_band = ["art", "leo", "zigaboo", "george"]
+  meters_band
 end
 
 def remove_element_from_start_of_array(array)
