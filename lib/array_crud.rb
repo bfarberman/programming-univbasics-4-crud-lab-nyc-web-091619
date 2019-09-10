a44339068c28eaa8e95653ef7d599506ca2f935d
Brad Farberman <bfarberman@gmail.com>
2019-09-10 00:41:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   meters_band = ["art", "leo", "zigaboo", "george"]
-  
+  meters_band.push
 end
 
 def add_element_to_start_of_array(array, element)
