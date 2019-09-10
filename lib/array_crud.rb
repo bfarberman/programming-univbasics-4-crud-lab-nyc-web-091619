5a7e6548d12539f68fe91611bdffc23e13ac40f7
Brad Farberman <bfarberman@gmail.com>
2019-09-10 00:52:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,8 +28,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   meters_band = ["art", "leo", "zigaboo", "george"]
-  
-  
+  meters_band["am"]
 end
 
 def retrieve_first_element_from_array(array)
